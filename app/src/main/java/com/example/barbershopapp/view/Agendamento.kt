65a4a8102abafefcee0384ae5e9b81fa8a035b95
LucasDevMelo <lucaslopesdemelosilva@gmail.com>
2023-06-17 11:43:21 -0300

    Atualização 17/06

--- app/src/main/java/com/example/barbershopapp/view/Agendamento.kt
+++ app/src/main/java/com/example/barbershopapp/view/Agendamento.kt
@@ -1,17 +1,24 @@
 package com.example.barbershopapp.view
 
+import android.graphics.Color
 import android.icu.util.Calendar
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
 import com.example.barbershopapp.R
 import com.example.barbershopapp.databinding.ActivityAgendamentoBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Agendamento : AppCompatActivity() {
 
     private lateinit var binding:ActivityAgendamentoBinding
-    privatre val calendar: Calendar = Calendar.getInstance()
+    private val calendar: Calendar = Calendar.getInstance()
     private var data: String=  ""
     private var hora: String = ""
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgendamentoBinding.inflate(layoutInflater)
@@ -22,7 +29,7 @@ class Agendamento : AppCompatActivity() {
 
         val datePicker = binding.datePicker
         datePicker.setOnDateChangedListener{ _, year, monthOfYear, dayOfMonth ->
-            calendar.setWeekData()
+            calendar.set(Calendar.YEAR,year)
             calendar.set(Calendar.MONTH, monthOfYear)
             calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
@@ -60,7 +67,54 @@ class Agendamento : AppCompatActivity() {
             val barbeiro2 = binding.barbeiro2
             val barbeiro3 = binding.barbeiro3
 
-            //54
+            when{
+                hora.isEmpty()-> {
+                    mensagem(it, "Preencha o horário", "#FF0000")
+                }
+                hora < "8:00" && hora >"19:00" -> {
+                    mensagem(it, "Barber shp está fechado - horário de atendimento das 08:00 às 19:00!", "#FF0000")
+                }
+                data.isEmpty() -> {
+                    mensagem(it, "Escolha uma data", "#FF0000")
+                }
+                barbeiro1.isChecked && data.isNotEmpty() && hora.isNotEmpty() ->{
+                    salvarAgendamento(it, nome, "Neymar Júnior", data,hora)
+                }
+                barbeiro2.isChecked && data.isNotEmpty() && hora.isNotEmpty() ->{
+                    salvarAgendamento(it, nome, "Giovana Gutierrez", data,hora)
+                }
+                barbeiro3.isChecked && data.isNotEmpty() && hora.isNotEmpty() ->{
+                    salvarAgendamento(it, nome, "Casimiro Miguel", data,hora)
+                }
+                else ->{
+                    mensagem(it, "Escolha um barbeiro!", "#FF0000")
+                }
+            }
+        }
+    }
+
+    private fun mensagem(view: View, mensagem: String, cor: String){
+        val snackbar = Snackbar.make(view, mensagem, Snackbar.LENGTH_SHORT)
+        snackbar.setBackgroundTint(Color.parseColor(cor))
+        snackbar.setTextColor(Color.parseColor("#FFFFFF"))
+        snackbar.show()
+    }
+
+    private fun salvarAgendamento(view: View, cliente: String, barbeiro: String, data:String, hora:String){
+
+        val db = FirebaseFirestore.getInstance()
+
+        val dadosUsuario = hashMapOf(
+            "cliente" to cliente,
+            "barbeiro" to barbeiro,
+            "data" to data,
+            "hora" to hora
+        )
+
+        db.collection("agendamento").document(cliente).set(dadosUsuario).addOnCompleteListener{
+            mensagem(view, "Agendamento realizado com sucesso!", "#FF03DAC5")
+        }.addOnFailureListener{
+            mensagem(view, "Erro no servidor!", "#FF0000")
         }
     }
 }
